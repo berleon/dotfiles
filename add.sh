bc050c072bd67cf83d73c4f03c0995447bcb3104
berleon <github@leon-sixt.de>
2016-01-01 16:58:14 +0100

    don't use dots

--- add.sh
+++ add.sh
@@ -3,9 +3,8 @@
 HOME=$(realpath ~)
 FILE=$(realpath $1)
 DOTFILES=$(realpath dotfiles)
-WITHOUT_HOME=${FILE#$HOME/}
+WITHOUT_HOME=${FILE#$HOME/.}
 DIR_PART=$(dirname $WITHOUT_HOME)
 echo "Adding $WITHOUT_HOME to dotfiles"
-echo $DIR_PART
 mkdir -p $DOTFILES/$DIR_PART
 cp $FILE $DOTFILES/${WITHOUT_HOME}
--- link.sh
+++ link.sh
@@ -1,14 +1,13 @@
 #! /usr/bin/env bash
 
-DOT_DIR=$(realpath dotfiles)
+DOT_DIR=$(realpath dotfiles/)
 DOTFILES=$(find $DOT_DIR -type f)
 HOME=$(realpath ~)
 echo $DOTFILES
 echo "############################# Link dotfiles ##################################"
 for file in $DOTFILES; do
-    DEST=$HOME/${file#$DOT_DIR}
-
-    echo "${file#DOT_DIR}"
+    DEST=$HOME/.${file#$DOT_DIR/}
+    echo ".${file#$DOT_DIR/}"
     ln -sf $file $DEST
 done
 
