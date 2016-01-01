6318f66331a83b1dfb59370c1bcc1c3b72f29985
berleon <github@leon-sixt.de>
2016-01-01 22:53:30 +0100

    add utility scripts

---
+++ scripts/mk-offline.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+
+wget \
+     --recursive \
+     --no-clobber \
+     --page-requisites \
+     --html-extension \
+     --convert-links \
+     --restrict-file-names=windows \
+     --domains $1 \
+     --no-parent \
+        $2
