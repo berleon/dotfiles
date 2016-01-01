36828d66eeeb1bd1b852e76ea4ccf97388a6f80f
berleon <github@leon-sixt.de>
2016-01-01 20:41:19 +0100

    backup existing files

--- link.sh
+++ link.sh
@@ -3,12 +3,18 @@
 DOT_DIR=$(realpath dotfiles/)
 DOTFILES=$(find $DOT_DIR -type f)
 HOME=$(realpath ~)
-echo $DOTFILES
+BACKUP=$(realpath backup)
+
 echo "############################# Link dotfiles ##################################"
 for file in $DOTFILES; do
     DEST=$HOME/.${file#$DOT_DIR/}
     echo ".${file#$DOT_DIR/}"
     mkdir -p `dirname $DEST`
+    if [ ! -L "$DEST" ]; then
+        BACKUP_DEST=$BACKUP/${DEST#$HOME}
+        mkdir -p `dirname $BACKUP_DEST`
+        cp $DEST $BACKUP_DEST
+    fi
     ln -sf $file $DEST
 done
 
