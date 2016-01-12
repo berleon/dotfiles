b7126e28ceba28c1ece8211b73bde60a77d02033
berleon <github@leon-sixt.de>
2016-01-12 13:34:41 +0100

    add vga extern monitor for automatic setup

--- scripts/setup_xrandr.sh
+++ scripts/setup_xrandr.sh
@@ -1,9 +1,26 @@
 #! /usr/bin/env bash
-xrandr | grep "DP2 connected" > /dev/null
-DP2=$?
-xrandr | grep "DP3 connected" > /dev/null
-DP3=$?
+
+function monitor_connected {
+    MONITOR=$1
+    xrandr | grep "$MONITOR connected" > /dev/null
+    echo $?
+}
+
+VGA=$(monitor_connected "VGA1")
+DP2=$(monitor_connected "DP2")
+DP3=$(monitor_connected "DP3")
+
+if [ "$1" == "test" ]; then
+    echo "VGA: $VGA"
+    echo "DP2: $DP2"
+    echo "DP3: $DP3"
+    exit
+fi
 
 if [ $DP2 ] && [ $DP3 ]; then
     xrandr_VHh.sh
+fi
+
+if [ $VGA ]; then
+    xrandr_vga_extern.sh
 fi
