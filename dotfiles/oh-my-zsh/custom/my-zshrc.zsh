522e82ac55ce182ec9501f57570021dbaf5cb756
berleon <github@leon-sixt.de>
2016-04-27 15:51:55 +0200

    Add ~/.local/bin to path

--- dotfiles/oh-my-zsh/custom/my-zshrc.zsh
+++ dotfiles/oh-my-zsh/custom/my-zshrc.zsh
@@ -1,7 +1,9 @@
 export PATH=/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin
 export PATH=$PATH:/usr/bin/core_perl:/usr/bin/vendor_perl/
 export PATH=$PATH:~/.cabal/bin
+export PATH=$PATH:~/.cargo/bin
 export PATH=$PATH:/opt/cuda/bin
+export PATH=$PATH:~/.local/bin/
 export EDITOR=vim
 export IDEA_JDK="/opt/oracle_jdk/"
 export LD_LIBRARY_PATH="/usr/local/lib/"
