eec1cf2425feab249614c87453567b6a98736342
berleon <github@leon-sixt.de>
2017-10-23 13:08:26 +0200

    Add script to connect to hn

---
+++ scripts/hn.sh
@@ -0,0 +1,29 @@
+#! /usr/bin/env bash
+
+if [ "$1" == "" ] ;
+then
+    HN="hn";
+else
+    HN="$1";
+fi
+
+LOCAL_CONFIG='~/.config/master_thesis'
+MONGO_HOST=$(ssh $HN "cat $LOCAL_CONFIG/mongodb_host")
+MONGO_PORT=$(ssh $HN "cat $LOCAL_CONFIG/mongodb_port")
+SACRED_BOARD_PORT=$(ssh $HN "cat $LOCAL_CONFIG/sacredboard_port")
+
+JUPYTER_HOST=$(ssh $HN "cat $LOCAL_CONFIG/jupyter_host")
+JUPYTER_PORT=$(ssh $HN "cat $LOCAL_CONFIG/jupyter_port")
+
+echo "MONGODB: $MONGO_HOST:$MONGO_PORT"
+echo "JUPYTER: $JUPYTER_HOST:$JUPYTER_PORT"
+
+CMD="ssh \
+	-L 27017:$MONGO_HOST:$MONGO_PORT \
+	-L 5000:$MONGO_HOST:$SACRED_BOARD_PORT \
+	-L 8000:$JUPYTER_HOST:$JUPYTER_PORT \
+	$HN
+	"
+
+echo $CMD
+$CMD
